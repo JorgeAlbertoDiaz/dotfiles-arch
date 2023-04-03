b806b902d0a3a865f26734c68b9beb6b715969f2
Jorge Alberto Díaz <Jorge.Alberto.Diaz@hotmail.com.ar>
2023-04-03 17:01:14 -0300

    feat (sway-bar): cambios en la barra de sway
    - Cambie los íconos de los espacios de trabajo (workspaces) y se agregué nuevos.
    - Puse el módulo de la hora en el centro.
    - active el módulo del clima y cambie la ubicación para obtenerlo (tafiviejotucuman).
    - puse en color negro el fondo de la barra de sway.

--- waybar/.config/waybar/scripts/weather.sh
+++ waybar/.config/waybar/scripts/weather.sh
@@ -7,7 +7,8 @@ content=$(curl -sS "https://thisdavej.azurewebsites.net/api/weather/current?loc=
 ICON=$(curl -s 'https://wttr.in/?format=1' | sed 's/[+0-9a-cA-Z°-]//g' )
 # echo $ICON
 TEMP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)"' | sed 's/"//g')
-TOOLTIP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)\n\(.skytext)"' | sed 's/"//g')
+# TOOLTIP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)\n\(.skytext)\n\(.observationpoint)"' | sed 's/"//g')
+TOOLTIP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType) - \n\(.observationpoint)"' | sed 's/"//g')
 CLASS=$(echo $content | jq .skytext)
-echo '{"text": "'$TEMP'", "tooltip": "'$ICON $TOOLTIP $LOC'", "class": '$CLASS' }'
+echo '{"text": "'$TEMP'", "tooltip": "'$ICON $TOOLTIP'", "class": '$CLASS' }'
 
